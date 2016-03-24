cf03617bef61feb758e1eb2638f21130bc2828d7
Mroosi <madisroosi@hotmail.com>
2016-03-24 22:30:24 +0200

    Kodune6 jQuery

---
+++ YL6/1/LoopJS.js
@@ -0,0 +1,38 @@
+﻿window.onload = function() {
+    var loendur = 0;
+    var list = $('li').get(loendur);
+    $(list).addClass("active");
+
+    $( '#next' ).click(function() {
+        if(loendur < 3){
+            loendur = loendur+1;
+            list = $('li').get(loendur-1);
+            $(list).removeClass("active");
+            list = $('li').get(loendur);
+            $(list).addClass("active");
+        }else{
+            loendur = 0;
+            list = $('li').last();
+            $(list).removeClass("active");
+            list = $('li').get(loendur);
+            $(list).addClass("active");
+        }
+    });
+
+    $( '#prev' ).click(function() {
+        if(loendur > 0){
+            loendur = loendur-1;
+            list = $('li').get(loendur+1);
+            $(list).removeClass("active");
+            list = $('li').get(loendur);
+            $(list).addClass("active");
+        }else{
+            loendur = 3;
+            list = $('li').first();
+            $(list).removeClass("active");
+            list = $('li').get(loendur);
+            $(list).addClass("active");
+        }
+    });
+
+}
---
+++ YL6/praktik6/LJS.js
@@ -0,0 +1,51 @@
+﻿var kuvaNpp = document.getElementById('kuvaVorm');
+var lisaVorm = document.getElementById('vorm');
+var paragrahv = document.getElementById('kuvaVormNupp');
+var peidaNupp = document.getElementById('peidaVorm');
+kuvaNpp.addEventListener('click', function(event) {
+    lisaVorm.style.display = 'block';
+    paragrahv.style.display = 'none';
+});
+peidaNupp.addEventListener('click', function(event) {
+    lisaVorm.style.display = 'none';
+    paragrahv.style.display = 'block';
+});
+lisaVorm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var nimetusElement = document.getElementById('nimetus');
+    var kogusElement = document.getElementById('kogus');
+    
+    if( ! nimetusElement.value || ! kogusElement.value ){
+        alert('Väljad täitmata!');
+        return;
+    }
+    
+    lisaRida(nimetusElement.value, kogusElement.value);
+    nimetusElement.value = '';
+    kogusElement.value = '';
+})
+function lisaRida(nimetus, kogus) {
+    var laotabel = document.querySelector('#laotabel tbody');
+    var rida = document.createElement('tr');
+    var nimetusTd = document.createElement('td');
+    var kogusTd = document.createElement('td');
+    var tegevusTd = document.createElement('td');
+    
+    var kustutaNupp = document.createElement('input');
+    kustutaNupp.setAttribute('type', 'button');
+    kustutaNupp.value = 'Kustuta rida';
+
+    tegevusTd.appendChild(kustutaNupp);
+    nimetusTd.textContent = nimetus;
+    kogusTd.textContent = kogus;
+    rida.appendChild(nimetusTd);
+    rida.appendChild(kogusTd);
+    rida.appendChild(tegevusTd);
+    laotabel.appendChild(rida);
+    
+    kustutaNupp.addEventListener('click', function(event) {
+        if(confirm('Kas oled kindel?')){
+            rida.parentNode.removeChild(rida);
+        }
+    });
+};
