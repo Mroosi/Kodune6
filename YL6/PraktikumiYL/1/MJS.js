50d7d2be8079153646007711083b15dda8df90ef
Mroosi <madisroosi@hotmail.com>
2016-03-27 23:01:16 +0300

    praktikumiyl

---
+++ YL6/PraktikumiYL/1/MJS.js
@@ -0,0 +1,8 @@
+﻿window.onload = function() {
+  
+    $('.click').click( function () {$('.click').children('ul').hide(), $(this).children().show();});
+    $('.hover').mouseenter( function () {$('.click').children('ul').hide(),$('.hover').children('ul').hide(), $(this).children().show();});
+    $('.hover').mouseleave( function () {$('.hover').children('ul').hide();});
+    
+   
+}
---
+++ YL6/PraktikumiYL/2/MJS.js
@@ -0,0 +1,5 @@
+﻿window.onload = function() {
+var menu = $('#menu > li');
+$(menu).mouseenter(function(){$(this).find('li').stop().show().animate({height: '20px',}, 'slow') });
+$(menu).mouseleave(function(){$(this).find('li').stop().show().animate({height: '0px',}, 'slow')});   
+}
